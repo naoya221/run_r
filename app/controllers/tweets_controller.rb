a4f5bdf54dde8fc1f2a2c92d998ccd53339c94d3
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-08-16 17:23:26 +0900

    投稿削除機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -20,6 +20,12 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to root_path
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:content)
