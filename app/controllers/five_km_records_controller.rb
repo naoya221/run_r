0eaec17c7010c3b913d0179e0b35df2b34d19eba
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-08-17 21:00:18 +0900

    five_km_recordsコントローラー作成

---
+++ app/controllers/five_km_records_controller.rb
@@ -0,0 +1,2 @@
+class FiveKmRecordsController < ApplicationController
+end
---
+++ test/controllers/five_km_records_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FiveKmRecordsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
