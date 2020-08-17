7f520f1728e45f5d5ad93c19c1dda95f5c162977
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-08-17 21:18:59 +0900

    five_km_recordsテーブルのカラム名変更

--- db/migrate/20200817110158_create_five_km_records.rb
+++ db/migrate/20200817110158_create_five_km_records.rb
@@ -1,9 +1,9 @@
 class CreateFiveKmRecords < ActiveRecord::Migration[6.0]
   def change
     create_table :five_km_records do |t|
-      t.integer   :hour
-      t.integer   :minute
-      t.integer   :second
+      t.integer   :hour_id,     null: false
+      t.integer   :minute_id,   null: false
+      t.integer   :second_id,   null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema.define(version: 2020_08_17_110158) do
 
   create_table "five_km_records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "hour"
-    t.integer "minute"
-    t.integer "second"
+    t.integer "hour_id", null: false
+    t.integer "minute_id", null: false
+    t.integer "second_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
