0f18862bf9109903c09956016ba0972b6d8453c2
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-08-17 21:26:32 +0900

    hourモデル作成（active_hash適用）

--- app/models/five_km_record.rb
+++ app/models/five_km_record.rb
@@ -1,2 +1,10 @@
 class FiveKmRecord < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :hour
+
+  #空の投稿を保存できないようにする
+  validates :hour, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :hour_id, numericality: { other_than: 1 } 
 end
