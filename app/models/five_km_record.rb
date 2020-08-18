1c4d3dcd90b94baac1b9934b25ea167f178c81d7
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-08-18 18:44:32 +0900

    5kmの時間のみ登録・更新機能完了

--- app/models/five_km_record.rb
+++ app/models/five_km_record.rb
@@ -3,9 +3,8 @@ class FiveKmRecord < ApplicationRecord
   belongs_to_active_hash :hour
   belongs_to :user  
 
-  #空の投稿を保存できないようにする
-  #validates :hour, presence: true
-
-  #ジャンルの選択が「--」の時は保存できないようにする
-  #validates :hour_id, numericality: { other_than: 1 } 
+  validates :user, uniqueness: true
+  validates :hour, presence: true
+  validates :hour_id, numericality: { other_than: 1 } 
+  
 end
