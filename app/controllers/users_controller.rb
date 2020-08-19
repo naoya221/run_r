c0719203569de5eb01183af206b6665b94717f4b
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-08-19 13:10:55 +0900

    全ての距離の自己ベストタイムをマイページに表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @nickname = @user.nickname
     @tweets = @user.tweets
+    @five_km_record = FiveKmRecord.find(params[:id])
+    @ten_km_record = TenKmRecord.find(params[:id])
+    @half_record = HalfRecord.find(params[:id])
+    @full_record = FullRecord.find(params[:id])
   end
 end
