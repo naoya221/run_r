68b8a26507fccac837ea695238cc05d92ebccd44
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-08-20 10:59:13 +0900

    vdotの指標一覧用モデル作成（active hash使用）

---
+++ app/models/vdot_record.rb
@@ -0,0 +1,2 @@
+class VdotRecord < ApplicationRecord
+end
---
+++ test/models/vdot_record_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class VdotRecordTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
