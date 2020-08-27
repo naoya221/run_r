aefce0c2725a389cc93b8d75680b95dab19a72bf
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-08-27 10:38:55 +0900

    各モデルのアソシエーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :tweets,  dependent: :destroy
-  has_many :messages,  dependent: :destroy
-  has_one :five_km_record,  dependent: :destroy
-  has_one :ten_km_record,  dependent: :destroy
-  has_one :half_record,  dependent: :destroy
-  has_one :full_record,  dependent: :destroy
+  has_many :tweets
+  has_many :messages
+  has_one :five_km_record
+  has_one :ten_km_record
+  has_one :half_record
+  has_one :full_record
   
 end
