089049c8be7cce5cdd91b173ce3957e6829a399e
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-08-27 11:16:55 +0900

    Revert "Revert "各モデルのアソシエーション修正""
    This reverts commit 29b8bdd8a2be3a620061d52c31977599c116d1c4.

--- app/models/ten_km_record.rb
+++ app/models/ten_km_record.rb
@@ -1,11 +1,9 @@
 class TenKmRecord < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :hour
   belongs_to_active_hash :minute
   belongs_to_active_hash :second
-  belongs_to :user  
+  belongs_to :user
 
-  validates :user, uniqueness: true
   
 end
