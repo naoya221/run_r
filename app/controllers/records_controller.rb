a45bf4beafa11d6be79586b8a25d187d4b59ace9
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-08-27 11:15:45 +0900

    ベストタイム編集のプルダウン初期表示修正

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -7,6 +7,24 @@ class RecordsController < ApplicationController
 
   def edit
     @user = User.find(current_user.id)
+
+    if FiveKmRecord.where(user_id: current_user.id).present?
+      five_record = FiveKmRecord.where(user_id: current_user.id).ids
+      @five_record = FiveKmRecord.find(five_record[0])
+    end
+    if TenKmRecord.where(user_id: current_user.id).present?
+      ten_record = TenKmRecord.where(user_id: current_user.id).ids
+      @ten_record = TenKmRecord.find(ten_record[0])
+    end
+    if HalfRecord.where(user_id: current_user.id).present?
+      half_record = HalfRecord.where(user_id: current_user.id).ids
+      @half_record = HalfRecord.find(half_record[0])
+    end
+    if FullRecord.where(user_id: current_user.id).present?
+      full_record = FullRecord.where(user_id: current_user.id).ids
+      @full_record = FullRecord.find(full_record[0])
+    end  
+
   end
 
 end
