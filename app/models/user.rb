1bcb00719ef5963fc5ed81c3f3af848fad74aa14
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-08-27 23:42:26 +0900

    password無しでnickname, email, 画像編集可能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -12,7 +25,7 @@ class User < ApplicationRecord
   has_one :full_record
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  
   validates :nickname, presence: true, length: { maximum: 10 }
 
   
