915f860be47974202b2733294c0d9886b81cba8a
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-08-28 15:38:04 +0900

    userモデルの単体テスト再度実施

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     email                 {"naoya@oosaki.com"}
     password              {"123abc"}
     password_confirmation {password}
+    image                 {"public/images/1.png"}
   end
 end
