ebc313d6c188d63e3be91d6ad916c2e9962ce857
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-09-01 10:32:22 +0900

    Revert "Merge pull request #16 from naoya221/不具合修正"
    This reverts commit 4400a4d79d3b4fc6d0efbfaef21276b8db70fe96, reversing
    changes made to 2d80abad90d3ed83c95b6cd0ef7eab9505a46308.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,10 +41,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.url = 'wss://run_r.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://run_r.com', /https:\/\/run_r.*/ ]
-  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
