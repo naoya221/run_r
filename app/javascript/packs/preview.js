28c8c73d0f1bcdd4edc37935963c5496384d298f
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-09-06 18:36:31 +0900

    ユーザー編集ページのプレビュー機能実装

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,12 +1,16 @@
-if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+if (document.URL.match( /new/ ) && document.URL.match( /tweets/ ) || document.URL.match( /edit/ ) && document.URL.match( /tweets/ ) || document.URL.match( /edit/ ) && document.URL.match( /users/ )) {
 
   document.addEventListener('DOMContentLoaded', function(){
     const imageList = document.getElementById('image-list');
     document.getElementById('preview').addEventListener('change', function(e){
 
       const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
+      if (document.URL.match( /new/ ) && document.URL.match( /tweets/ )|| document.URL.match( /edit/ ) && document.URL.match( /tweets/ )){
+
+        if (imageContent){
+          imageContent.remove();
+        }
+
       }
 
       const file = e.target.files[0];
