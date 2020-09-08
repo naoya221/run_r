0c44801b7e38de7d4454c3610a4c7837fe496cf7
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-09-08 16:29:53 +0900

    ベストタイムの各モデルのテストを修正

--- spec/models/full_record_spec.rb
+++ spec/models/full_record_spec.rb
@@ -15,22 +15,22 @@ describe FullRecord do
       it "ベストタイム（時間）が空だと保存できない" do
         @full_record.hour_id = ""
         @full_record.valid?
-        expect(@full_record.errors.full_messages).to include("Hour can't be blank")
+        expect(@full_record.errors.full_messages).to include("Hourを入力してください")
       end   
       it "ベストタイム（分）が空だと保存できない" do
         @full_record.minute_id = ""
         @full_record.valid?
-        expect(@full_record.errors.full_messages).to include("Minute can't be blank")
+        expect(@full_record.errors.full_messages).to include("Minuteを入力してください")
       end     
       it "ベストタイム（秒）が空だと保存できない" do
         @full_record.second_id = ""
         @full_record.valid?
-        expect(@full_record.errors.full_messages).to include("Second can't be blank")
+        expect(@full_record.errors.full_messages).to include("Secondを入力してください")
       end
       it "登録したユーザーが紐付いていないとベストタイムは保存できない" do
         @full_record.user = nil
         @full_record.valid?
-        expect(@full_record.errors.full_messages).to include("User must exist")
+        expect(@full_record.errors.full_messages).to include("Userを入力してください")
       end
     end
   end
