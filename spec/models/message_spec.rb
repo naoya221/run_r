abfd134b6c8e86a658549dec5f96730fda952f74
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-09-08 16:29:32 +0900

    messageモデルのエラー文を日本語化、テスト修正

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -15,17 +15,17 @@ describe Message do
       it "テキストが空だとコメントは保存できない" do
         @message.text = ""
         @message.valid?
-        expect(@message.errors.full_messages).to include("Text can't be blank")
+        expect(@message.errors.full_messages).to include("テキストを入力してください")
       end     
       it "ユーザーが紐付いていないとコメントは保存できない" do
         @message.user = nil
         @message.valid?
-        expect(@message.errors.full_messages).to include("User must exist")
+        expect(@message.errors.full_messages).to include("Userを入力してください")
       end
       it "コメントされている投稿が紐付いていないとコメントは保存できない" do
         @message.tweet = nil
         @message.valid?
-        expect(@message.errors.full_messages).to include("Tweet must exist")
+        expect(@message.errors.full_messages).to include("Tweetを入力してください")
       end
     end
   end
