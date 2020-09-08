54697b942a4e1b63ae731ba11750e606c1a8ab5a
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-09-08 15:10:01 +0900

    tweetモデルテスト追加

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,7 +1,10 @@
 FactoryBot.define do
   factory :tweet do
-    content     {Faker::Lorem.sentence}
-    
+    content     {Faker::Name.initials(number: 10)}
+    address       {"東京都千代田区千代田1-1"}
+    latitude      {35.6837}
+    longitude     {139.629}
+    place_name    {Faker::Name.initials(number: 10)}
     association :user
   end
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -2,25 +2,49 @@ require 'rails_helper'
 describe Tweet do
   before do
     @tweet = FactoryBot.build(:tweet)
+    @tweet.place_image = fixture_file_upload("public/images/place_image.png")
   end
 
   describe '投稿の保存' do
     context "投稿が保存できる場合" do
-      it "テキストがあれば保存される" do
+      it "全てのフォームに情報が入力されていれば投稿できる" do
+        expect(@tweet).to be_valid
+      end
+      it "緯度がなくても投稿できる" do
+        @tweet.latitude = ""
+        expect(@tweet).to be_valid
+      end
+      it "経度がなくても投稿できる" do
+        @tweet.longitude = ""
         expect(@tweet).to be_valid
       end
     end
 
     context "投稿が保存できない場合" do
-      it "テキストが空だと投稿は保存できない" do
+      it "場所の名前が空だと投稿は保存できない" do
+        @tweet.place_name = ""
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include("場所の名前を入力してください")
+      end  
+      it "場所の画像が空だと投稿は保存できない" do
+        @tweet.place_image = ""
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include("場所の画像を入力してください")
+      end  
+      it "住所が空だと投稿は保存できない" do
+        @tweet.address = ""
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include("住所を入力してください")
+      end  
+      it "何かひとことが空だと投稿は保存できない" do
         @tweet.content = ""
         @tweet.valid?
-        expect(@tweet.errors.full_messages).to include("Content can't be blank")
+        expect(@tweet.errors.full_messages).to include("　何かひとことを入力してください")
       end     
       it "ユーザーが紐付いていないと投稿は保存できない" do
         @tweet.user = nil
         @tweet.valid?
-        expect(@tweet.errors.full_messages).to include("User must exist")
+        expect(@tweet.errors.full_messages).to include("Userを入力してください")
       end
     end
   end
