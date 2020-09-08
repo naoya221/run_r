3250e131607837a27252178f4edb4f51e863ab87
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-09-08 20:04:08 +0900

    ランニングコース投稿機能の結合テスト追加

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :tweet do
-    content     {Faker::Name.initials(number: 10)}
-    address       {"東京都千代田区千代田1-1"}
+    content       {Faker::Name.initials(number: 10)}
+    place_image   { Rack::Test::UploadedFile.new(File.join(Rails.root, 'public/images/dog2.png')) }
+    address       {Faker::Name.initials(number: 10)}
     latitude      {35.6837}
     longitude     {139.629}
     place_name    {Faker::Name.initials(number: 10)}
