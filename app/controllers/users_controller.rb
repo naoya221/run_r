01fc4258193d120a193a9b90ccafa120c7586393
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-09-13 11:19:24 +0900

    ユーザー管理機能のコントローラーにてコードを省略化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,46 +1,26 @@
 class UsersController < ApplicationController
-  before_action :params_present
-  before_action :set_params
-  before_action :set_nickname
 
   def show
+    params_present
+    @user = User.find(params[:id])
     @tweets = @user.tweets.page(params[:page]).per(8).order('created_at DESC')
   end
 
   private
 
-  def set_params
-    @user = User.find(params[:id])
-  end
-
-  def set_nickname
-    @nickname = @user.nickname
-  end
-
-  def permit_params
-    params.require(:user).permit(:nickname)
-  end
-
   def params_present
     if FiveKmRecord.where(user_id: params[:id]).present?
-      five_km_record_id_hash = FiveKmRecord.where(user_id: params[:id])
-      five_km_record_id = five_km_record_id_hash.ids
-      @five_km_record = FiveKmRecord.find(five_km_record_id[0])
+      @five_record = FiveKmRecord.find_by(user_id: params[:id])
     end
     if TenKmRecord.where(user_id: params[:id]).present?
-      ten_km_record_id_hash = TenKmRecord.where(user_id: params[:id])
-      ten_km_record_id = ten_km_record_id_hash.ids
-      @ten_km_record = TenKmRecord.find(ten_km_record_id[0])
+      @ten_record = TenKmRecord.find_by(user_id: params[:id])
     end
     if HalfRecord.where(user_id: params[:id]).present?
-      half_record_id_hash = HalfRecord.where(user_id: params[:id])
-      half_record_id = half_record_id_hash.ids
-      @half_record = HalfRecord.find(half_record_id[0])
+      @half_record = HalfRecord.find_by(user_id: params[:id])
     end
     if FullRecord.where(user_id: params[:id]).present?
-      full_record_id_hash = FullRecord.where(user_id: params[:id])
-      full_record_id = full_record_id_hash.ids
-      @full_record = FullRecord.find(full_record_id[0])
+      @full_record = FullRecord.find_by(user_id: params[:id])
     end
   end
+
 end
