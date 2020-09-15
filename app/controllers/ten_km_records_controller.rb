511e3b119a22a2f41b357201c32b070eddb75fd6
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-09-15 22:03:09 +0900

    Update ten_km_records_controller.rb

--- app/controllers/ten_km_records_controller.rb
+++ app/controllers/ten_km_records_controller.rb
@@ -23,9 +23,9 @@ class TenKmRecordsController < ApplicationController
   def destroy
     set_record
     if @ten_record.destroy
-      redirect_to edit_record_path
+      redirect_to edit_record_path, notice: '10kmのベストタイムを削除しました！'
     else
-      redirect_to root_path, notice: '10kmのベストタイムを削除しました！'
+      redirect_to root_path
     end
   end
 
