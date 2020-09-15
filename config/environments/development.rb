d7cf71bff3f9d0e14f2543faae507b4809ce76de
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-09-15 15:38:25 +0900

    ローカルのコード変更されないため、docker・cicleCIファイル削除

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # dockerコンテナでのコード変更を即時反映
+  # config.reload_classes_only_on_change = false
+
 end
