aa09430d034bc38fc0826788db472cbec98fe992
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-09-24 15:45:07 +0900

    S3に画像が保存されないため

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,14 @@
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
-    }
-    config.fog_directory  = '32naoya25'
-    config.cache_storage = :fog     # 本番時はS3にファイルを保存する
-  else
+  # if Rails.env.production?
+  #   config.fog_credentials = {
+  #     provider: 'AWS',
+  #     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  #     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  #     region: 'ap-northeast-1'
+  #   }
+  #   config.fog_directory  = '32naoya25'
+  #   config.cache_storage = :fog     # 本番時はS3にファイルを保存する
+  # else
     config.storage = :file          # 開発・テスト時はローカルにファイルを保存する
   end
 end
