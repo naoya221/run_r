612d642b5b8d20ed4eb3a3e398b2b8db47f50980
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-09-24 11:31:20 +0900

    circle ci上でmaster.keyの環境変数設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,11 @@
-# config valid for current version and patch releases of Capistrano
-# lock "~> 3.14.1"
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.14.1'
 
-# set :application, "my_app_name"
-# set :repo_url, "git@example.com:me/my_repo.git"
+# Capistranoのログの表示に利用する
+set :application, 'run_r'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:naoya221/run_r.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -38,24 +41,14 @@
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '3.14.1'
-
-# Capistranoのログの表示に利用する
-set :application, 'run_r'
-
-# どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:naoya221/run_r.git'
-
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/naoya.pem'] 
+                                  keys: ['~/.ssh/naoya.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
