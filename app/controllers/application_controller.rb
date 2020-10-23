68f0e5d951667cadaf01dffa5dd3598b77d7086b
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-10-23 14:21:03 +0900

    検索方法に応じて、表示を切り替える

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery except: [:destroy]
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :request_path
 
-  private
+  # 特定のアクションのみ、何か表示する際に使用
+  def request_path
+    @path = controller_path + '#' + action_name
+    def @path.is(*str)
+        str.map{|s| self.include?(s)}.include?(true)
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :image, :introduction])
