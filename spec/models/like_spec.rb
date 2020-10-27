e386e0e98b21b6fb06033de27f786ad2163dc488
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-10-27 17:53:17 +0900

    likeモデルのアソシエーションテスト

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
+describe Like do
+  describe 'アソシエーション' do
+    let(:association) do
+       described_class.reflect_on_association(target)
+    end
 
-RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    context 'TWeetモデルとの関係' do
+      let(:target) { :tweet }
+      it '多:1' do
+        expect(association.macro).to eq :belongs_to
+      end
+      it '結合するモデルのクラス名：Tweet' do
+        expect(association.class_name).to eq 'Tweet'
+      end
+    end
+
+    context 'Userモデルとの関係' do
+      let(:target) { :user }
+      it '多:1' do
+        expect(association.macro).to eq :belongs_to
+      end
+      it '結合するモデルのクラス名：User' do
+        expect(association.class_name).to eq 'User'
+      end
+    end
+  end
 end
