6a5e08a1438122d3c86385479b18e46fa386785f
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-10-27 17:02:56 +0900

    userモデルのアソシエーションテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+
 describe User do
   before do
     @user = FactoryBot.build(:user)
@@ -80,6 +81,112 @@ describe User do
         @user.introduction = "a" * 101
         @user.valid?
         expect(@user.errors.full_messages).to include('自己紹介文は100文字以内で入力してください')
+      end
+    end
+  end
+
+  describe 'アソシエーション' do
+    let(:association) do
+       described_class.reflect_on_association(target)
+    end
+
+    context 'Tweetモデルとの関係' do
+      let(:target) { :tweets }
+      it '1:多' do
+        expect(association.macro).to eq :has_many
+      end
+      it '結合するモデルのクラス名：Tweet' do
+        expect(association.class_name).to eq 'Tweet'
+      end
+    end
+
+    context 'Likeモデルとの関係' do
+      let(:target) { :likes }
+      it '1:多' do
+        expect(association.macro).to eq :has_many
+      end
+      it '結合するモデルのクラス名：Like' do
+        expect(association.class_name).to eq 'Like'
+      end
+    end
+
+    context 'FollowRelationship（following_relationships）モデルとの関係' do
+      let(:target) { :following_relationships }
+      it '1:多' do
+        expect(association.macro).to eq :has_many
+      end
+      it '結合するモデルのクラス：FollowRelationship' do
+        expect(association.class_name).to eq 'FollowRelationship'
+      end
+    end
+
+    context 'FollowRelationship（follower_relationships）モデルとの関係' do
+      let(:target) { :follower_relationships }
+      it '1:多' do
+        expect(association.macro).to eq :has_many
+      end
+      it '結合するモデルのクラス：FollowRelationship' do
+        expect(association.class_name).to eq 'FollowRelationship'
+      end
+    end
+
+    context 'Notificationモデル（active_notifications）との関係' do
+      let(:target) { :active_notifications }
+      it '1:多' do
+        expect(association.macro).to eq :has_many
+      end
+      it '結合するモデルのクラス：Notification' do
+        expect(association.class_name).to eq 'Notification'
+      end
+    end
+
+    context 'Notificationモデル（passive_notifications）との関係' do
+      let(:target) { :passive_notifications }
+      it '1:多' do
+        expect(association.macro).to eq :has_many
+      end
+      it '結合するモデルのクラス：Notification' do
+        expect(association.class_name).to eq 'Notification'
+      end
+    end
+
+    context 'FiveKmRecordモデルとの関係' do
+      let(:target) { :five_km_record }
+      it '1:1' do
+        expect(association.macro).to eq :has_one
+      end
+      it '結合するモデルのクラス：FiveKmRecord' do
+        expect(association.class_name).to eq 'FiveKmRecord'
+      end
+    end
+
+    context 'TenKmRecordモデルとの関係' do
+      let(:target) { :ten_km_record }
+      it '1:1' do
+        expect(association.macro).to eq :has_one
+      end
+      it '結合するモデルのクラス：TenRecord' do
+        expect(association.class_name).to eq 'TenKmRecord'
+      end
+    end
+
+    context 'HalfRecordモデルとの関係' do
+      let(:target) { :half_record }
+      it '1:1' do
+        expect(association.macro).to eq :has_one
+      end
+      it '結合するモデルのクラス：HalfRecord' do
+        expect(association.class_name).to eq 'HalfRecord'
+      end
+    end
+
+    context 'FullRecordモデルとの関係' do
+      let(:target) { :full_record }
+      it '1:1' do
+        expect(association.macro).to eq :has_one
+      end
+      it '結合するモデルのクラス：FullRecord' do
+        expect(association.class_name).to eq 'FullRecord'
       end
     end
   end
