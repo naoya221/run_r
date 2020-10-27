9e67a0a3350adefd3793208e8a13674368394369
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-10-27 18:00:56 +0900

    不要ファイル・余白の削除

--- spec/factories/notifications.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :notification do
-    visitor { nil }
-    visited { nil }
-    tweet { nil }
-    action { 'MyString' }
-    checked { false }
-  end
-end
--- spec/factories/tag_tweets.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :tag_tweet do
-    tweet { nil }
-    tag { nil }
-  end
-end
