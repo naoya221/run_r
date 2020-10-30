cf2dc482307ae1d5ed61136d9d818810837f010a
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-10-30 17:10:33 +0900

    最大走力レベルの計算部分をリファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,22 +28,12 @@ class UsersController < ApplicationController
 
   private
 
-  def runner_title
+  def runner_title    # 走力に応じた称号を取得
     vdots = [@five_vdot, @ten_vdot, @half_vdot, @full_vdot]
     max_vdot = vdots.max
 
-    if max_vdot >= 25
-      @runner_level_num = 0
-    elsif max_vdot >= 20
-      @runner_level_num = 1
-    elsif max_vdot >= 15
-      @runner_level_num = 2
-    elsif max_vdot >= 10
-      @runner_level_num = 3
-    elsif max_vdot >= 5
-      @runner_level_num = 4
-    else
-      @runner_level_num = 5
-    end
+    # 最大走力レベルに対応する称号を取得
+    # (計算方法)          5 - (最大走力レベル ÷5 )(端数切捨て) ただし0より小さくはならないようにする
+    @runner_level_num = [5 - (max_vdot / 5).to_i, 0].max
   end
 end
