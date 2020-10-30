d4a0ec534d134cb10d534ae83c57cd7dc9dc33a2
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-10-30 20:04:40 +0900

    通知が来ない不具合の修正

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,5 @@
 class Notification < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
-  ACTION_VALUES = %w[like follow message].freeze
 
   # ----- アソシエーション -----------------------------------
     belongs_to :visitor, class_name: 'User', optional: true
@@ -10,11 +9,9 @@ class Notification < ApplicationRecord
 
 
   # ----- バリデーション ------------------------------------
-    # with_options presence: true do
-      validates :visitor_id
-      validates :visited_id
-      validates :checked, inclusion: { in: [true, false] }
-    # end
-    #  ACTION_VALUESを適用するため、上記ブロックから出す
+    validates :visitor_id, presence: true
+    validates :visited_id, presence: true
+    ACTION_VALUES = ['like', 'follow', 'message']
     validates :action, presence: true, inclusion: { in: ACTION_VALUES }
+    validates :checked, inclusion: { in: [true, false] }
 end
