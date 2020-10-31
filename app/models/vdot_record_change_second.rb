c7bf999fdcc992678ed72a157b6dcaa4e94a80c8
naoya221 <68312425+naoya221@users.noreply.github.com>
2020-10-31 15:49:13 +0900

    走力レベルの指標（秒数変換）を追加

---
+++ app/models/vdot_record_change_second.rb
@@ -0,0 +1,32 @@
+class VdotRecordChangeSecond < ActiveHash::Base
+  self.data = [
+    { id: 1, five_record_from: 6000, five_record_to: 1840, ten_record_from: 6000, ten_record_to: 3828, half_record_from:42000, half_record_to: 8464, full_record_from: 42000, full_record_to: 17357 },
+    { id: 2, five_record_from: 1840, five_record_to: 1791, ten_record_from: 3823, ten_record_to: 3723, half_record_from:8464, half_record_to: 8241, full_record_from: 17357, full_record_to: 16917 },
+    { id: 3, five_record_from: 1791, five_record_to: 1745, ten_record_from: 3723, ten_record_to: 3626, half_record_from:8241, half_record_to: 8029, full_record_from: 16917, full_record_to: 16474 },
+    { id: 4, five_record_from: 1745, five_record_to: 1701, ten_record_from: 3626, ten_record_to: 3534, half_record_from:8029, half_record_to: 7827, full_record_from: 16474, full_record_to: 16102 },
+    { id: 5, five_record_from: 1701, five_record_to: 1659, ten_record_from: 3534, ten_record_to: 3446, half_record_from:7827, half_record_to: 7636, full_record_from: 16102,  full_record_to: 15723 },
+    { id: 6, five_record_from: 1659, five_record_to: 1620, ten_record_from: 3446, ten_record_to: 3363, half_record_from: 7636, half_record_to: 7457, full_record_from: 15723, full_record_to: 15363 },
+    { id: 7, five_record_from: 1620, five_record_to: 1582, ten_record_from: 3363, ten_record_to: 3284, half_record_from: 7457, half_record_to: 7279, full_record_from: 15363, full_record_to: 15019 },
+    { id: 8, five_record_from: 1582, five_record_to: 1546, ten_record_from: 3284, ten_record_to: 3209, half_record_from: 7279, half_record_to: 7114, full_record_from: 15019, full_record_to: 14690 },
+    { id: 9, five_record_from: 1546, five_record_to: 1512, ten_record_from: 3209, ten_record_to: 3137, half_record_from: 7114, half_record_to: 6955, full_record_from: 14690, full_record_to: 14375 },
+    { id: 10, five_record_from: 1512, five_record_to: 1479, ten_record_from: 3137, ten_record_to: 3069, half_record_from: 6955, half_record_to: 6804, full_record_from: 14375, full_record_to: 14074 },
+
+    { id: 11, five_record_from: 1479, five_record_to: 1448, ten_record_from: 3069, ten_record_to: 3003, half_record_from: 6804, half_record_to: 6659, full_record_from: 14074, full_record_to: 13785 },
+    { id: 12, five_record_from: 1448, five_record_to: 1418, ten_record_from: 3003, ten_record_to: 2941, half_record_from: 6659, half_record_to: 6520, full_record_from: 13785, full_record_to: 13509 },
+    { id: 13, five_record_from: 1418, five_record_to: 1389, ten_record_from: 2941, ten_record_to: 2881, half_record_from: 6520, half_record_to: 6387, full_record_from: 13509, full_record_to: 13243 },
+    { id: 14, five_record_from: 1389, five_record_to: 1361, ten_record_from: 2881, ten_record_to: 2824, half_record_from: 6387, half_record_to: 6260, full_record_from: 13243, full_record_to: 12988 },
+    { id: 15, five_record_from: 1361, five_record_to: 1335, ten_record_from: 2824, ten_record_to: 2769, half_record_from: 6260, half_record_to: 6137, full_record_from: 12988, full_record_to: 12743 },
+    { id: 16, five_record_from: 1335, five_record_to: 1310, ten_record_from: 2769, ten_record_to: 2716, half_record_from: 6137, half_record_to: 6020, full_record_from: 12743, full_record_to: 12506 },
+    { id: 17, five_record_from: 1310, five_record_to: 1285, ten_record_from: 2716, ten_record_to: 2665, half_record_from: 6020, half_record_to: 5907, full_record_from: 12506, full_record_to: 12279 },
+    { id: 18, five_record_from: 1285, five_record_to: 1262, ten_record_from: 2665, ten_record_to: 2616, half_record_from: 5907, half_record_to: 5798, full_record_from: 12279, full_record_to: 12060 },
+    { id: 19, five_record_from: 1262, five_record_to: 1239, ten_record_from: 2616, ten_record_to: 2570, half_record_from: 5798, half_record_to: 5693, full_record_from: 12060, full_record_to: 11849 },
+    { id: 20, five_record_from: 1239, five_record_to: 1218, ten_record_from: 2570, ten_record_to: 2524, half_record_from: 5693, half_record_to: 5592, full_record_from: 11849, full_record_to: 11646 },
+
+    { id: 21, five_record_from: 1218, five_record_to: 1197, ten_record_from: 2524, ten_record_to: 2481, half_record_from: 5592, half_record_to: 5495, full_record_from: 11646, full_record_to: 11449 },
+    { id: 22, five_record_from: 1197, five_record_to: 1176, ten_record_from: 2481, ten_record_to: 2439, half_record_from: 5495, half_record_to: 5402, full_record_from: 11449, full_record_to: 11259 },
+    { id: 23, five_record_from: 1176, five_record_to: 1157, ten_record_from: 2439, ten_record_to: 2399, half_record_from: 5402, half_record_to: 5311, full_record_from: 11259, full_record_to: 11076 },
+    { id: 24, five_record_from: 1157, five_record_to: 1138, ten_record_from: 2399, ten_record_to: 2361, half_record_from: 5311, half_record_to: 5224, full_record_from: 11076, full_record_to: 10899 },
+    { id: 25, five_record_from: 1138, five_record_to: 1120, ten_record_from: 2361, ten_record_to: 2322, half_record_from: 5224, half_record_to: 5140, full_record_from: 10899, full_record_to: 10727 },
+    { id: 25, five_record_from: 1120, five_record_to: 0, ten_record_from: 2322, ten_record_to: 0, half_record_from: 5140, half_record_to: 0, full_record_from: 10727, full_record_to: 0 }
+  ]
+  end
